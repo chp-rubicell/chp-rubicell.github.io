576d058a9a01b7f1450be8b6b9190dfa07953658
Rubicell <rubicell741@gmail.com>
2025-06-14 02:30:58 +0900

    design tweak

--- resources/scripts.js
+++
@@ -1,9 +0,0 @@
-function toggleColorTheme() {
-    if (document.documentElement.getAttribute('colorTheme') == 'dark') {
-        document.documentElement.setAttribute('colorTheme', 'light');
-    }
-    else {
-        document.documentElement.setAttribute('colorTheme', 'dark');
-    }
-}
-
